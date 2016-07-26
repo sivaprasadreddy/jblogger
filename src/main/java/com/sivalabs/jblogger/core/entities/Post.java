ea598fe8ed9f1ca286ba0486dc1bd4380e40bbfa
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2016-07-26 19:02:39 +0530

    Updating boot version to 1.4.0.RC1.

--- src/main/java/com/sivalabs/jblogger/core/entities/Post.java
+++ src/main/java/com/sivalabs/jblogger/core/entities/Post.java
@@ -17,7 +17,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -25,7 +24,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.google.common.base.Objects;
