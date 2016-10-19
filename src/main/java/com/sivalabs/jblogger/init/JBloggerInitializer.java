9eddebb77f8824fe5cf2b50fdad2e47e4230c5b5
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2016-10-19 11:12:56 +0530

    Removing Solr support.

--- src/main/java/com/sivalabs/jblogger/init/JBloggerInitializer.java
+++ src/main/java/com/sivalabs/jblogger/init/JBloggerInitializer.java
@@ -5,13 +5,9 @@ package com.sivalabs.jblogger.init;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
-import com.sivalabs.jblogger.core.services.PostService;
-import com.sivalabs.jblogger.search.BlogSearch;
 
 /**
  * @author Siva
@@ -22,15 +18,11 @@ public class JBloggerInitializer
 {
 	private static final Logger logger = LoggerFactory.getLogger(JBloggerInitializer.class); 
 	
-	@Autowired BlogSearch blogSearch;
-	@Autowired PostService postService;
-	
-	//ApplicationReadyEvent
 	@EventListener
     public void handleContextRefresh(ContextRefreshedEvent event) {
 		logger.info("Received ContextRefreshedEvent "+event.getTimestamp());
 		logger.info("Initializing JBlogger...........");
-		blogSearch.index(postService.findAllPosts());
+		//TODO;
 		logger.info("Done.");
     }
 }
